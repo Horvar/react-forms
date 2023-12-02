37e056aa4683726615caa2373f3e842af01e01d6
Калашников Семен <horselvaren@gmail.com>
2023-12-02 16:14:00 +0600

    chore: update lint config

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -9,15 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://reeact.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      qwe
+      <h1>Vite + Reeact</h1>
       <div className="card">
+        eqe
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
