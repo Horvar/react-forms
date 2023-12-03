c5e38afd3bbf4d4ccad39bb9673ed78959637f8c
Калашников Семен <horselvaren@gmail.com>
2023-12-03 16:12:44 +0600

    feat: add validation for ControlledFormPage

--- react-app/src/pages/ControlledFormPage/index.tsx
+++ react-app/src/pages/ControlledFormPage/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useDispatch } from 'react-redux';
 import { setControlledFormData } from '../../store/formSlice';
+import validationSchema from '../../validationScheme'; // Убедитесь, что путь к файлу схемы валидации верный
 
 interface FormData {
   name: string;
@@ -16,7 +18,9 @@ interface FormData {
 
 const ControlledFormPage: React.FC = () => {
   const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: yupResolver(validationSchema)
+  });
   const [pictureBase64, setPictureBase64] = useState<string>('');
 
   const handlePictureChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,22 +47,27 @@ const ControlledFormPage: React.FC = () => {
       <label>
         <span>Имя:</span>
         <input {...register("name")} type="text" />
+        {errors.name && <p>{errors.name.message}</p>}
       </label>
       <label>
         <span>Возраст:</span>
         <input {...register("age")} type="number" />
+        {errors.age && <p>{errors.age.message}</p>}
       </label>
       <label>
         <span>Email:</span>
         <input {...register("email")} type="email" />
+        {errors.email && <p>{errors.email.message}</p>}
       </label>
       <label>
         <span>Пароль:</span>
         <input {...register("password")} type="password" />
+        {errors.password && <p>{errors.password.message}</p>}
       </label>
       <label>
         <span>Подтвердите Пароль:</span>
         <input {...register("confirmPassword")} type="password" />
+        {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
       </label>
       <label>
         <span>Пол:</span>
@@ -71,10 +80,12 @@ const ControlledFormPage: React.FC = () => {
       <label>
         <span>Согласие с условиями:</span>
         <input {...register("termsAccepted")} type="checkbox" />
+        {errors.termsAccepted && <p>{errors.termsAccepted.message}</p>}
       </label>
       <label>
         <span>Страна:</span>
         <input {...register("country")} type="text" />
+        {errors.country && <p>{errors.country.message}</p>}
       </label>
       <label>
         <span>Загрузите изображение:</span>
